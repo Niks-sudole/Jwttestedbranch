0ef0ccfc1c8f0fe2ce5d989ecd7a2259aa78b093
Niks-sudole <suryanitu1234@gmail.com>
2024-12-27 23:02:36 +0530

    Add files via upload
    jwt_swagger_included_changes

---
+++ Umm-project/demo/src/main/java/com/example/demo/UmmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmmProjectApplication.class, args);
+	}
+
+}
